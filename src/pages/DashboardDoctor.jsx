98374e552447992707d81e807b7056a5739532f9
ShowyCone <alexrafael2003@gmail.com>
2025-04-27 02:55:14 -0400

    Refactor DashboardDoctor layout for improved header organization

--- src/pages/DashboardDoctor.jsx
+++ src/pages/DashboardDoctor.jsx
@@ -21,17 +21,22 @@ const DashboardDoctor = () => {
 
   return (
     <>
-      <header className='flex justify-end p-5 gap-5 w-full bg-background-light dark:bg-background-dark'>
-        <Link
-          to='/login'
-          onClick={() => logout()}
-          className='flex justify-center items-center gap-3 font-semibold text-secondary'
-        >
-          Log Out <FaArrowRightFromBracket />
-        </Link>
-        <ThemeSwitch />
+      <header className='flex justify-between p-5 gap-5 w-full bg-background-light dark:bg-background-dark'>
+        <h1 className='font-bold text-2xl text-text-light dark:text-text-dark'>
+          Medical Record Dashboard
+        </h1>
+        <div className='flex items-center gap-5'>
+          <Link
+            to='/login'
+            onClick={() => logout()}
+            className='flex justify-center items-center gap-3 font-semibold text-secondary'
+          >
+            Log Out <FaArrowRightFromBracket />
+          </Link>
+          <ThemeSwitch />
+        </div>
       </header>
-      <main className='relative min-h-[calc(100vh-73.99px)] bg-background-light text-text-light dark:bg-background-dark dark:text-text-dark px-24 py-5 gap-10 flex flex-col justify-start'>
+      <main className='relative min-h-[calc(100vh-73.99px)] bg-background-light text-text-light dark:bg-background-dark dark:text-text-dark px-24 py-5'>
         <Modal isOpen={isOpen} onClose={closeModal}>
           <button
             onClick={closeModal}
@@ -41,36 +46,33 @@ const DashboardDoctor = () => {
           </button>
           <FormPatient closeModal={closeModal} />
         </Modal>
-        <div className='flex justify-between items-center'>
-          <h1 className='font-bold text-2xl text-text-light dark:text-text-dark'>
-            Medical Record Dashboard
-          </h1>
-          <div className='flex gap-4'>
-            <button
-              className='p-3 text-white font-semibold rounded-xl bg-gradient-to-r from-primary to-secondary flex items-center gap-2 border-slate-400 border cursor-pointer
+        <div className='flex flex-col justify-center w-full h-full grow'>
+          <div className='flex justify-between items-center'>
+            <PatientSearchBar filter={filter} setFilter={setFilter} />
+            <div className='flex gap-4'>
+              <button
+                className='p-3 text-white font-semibold rounded-xl bg-gradient-to-r from-primary to-secondary flex items-center gap-2 border-slate-400 border cursor-pointer
               hover:scale-105 transition-transform duration-300 
               hover:shadow-lg hover:shadow-secondary/50 
               hover:outline-2 hover:outline-white 
               hover:bg-opacity-80 hover:animate-pulse'
-              onClick={openModal}
-            >
-              <FaPlus />
-              Add New Record
-            </button>
-            <button
-              className='p-3 text-white font-semibold rounded-xl bg-[#10793F] flex items-center gap-2 border-slate-400 border cursor-pointer
+                onClick={openModal}
+              >
+                <FaPlus />
+                Add New Record
+              </button>
+              <button
+                className='p-3 text-white font-semibold rounded-xl bg-[#10793F] flex items-center gap-2 border-slate-400 border cursor-pointer
               hover:scale-105 transition-transform duration-300 
               hover:shadow-lg hover:shadow-[#10793F]/50 
               hover:outline-2 hover:outline-white 
               hover:bg-opacity-80 hover:animate-pulse'
-            >
-              <FaFileExport />
-              Import From Excel
-            </button>
+              >
+                <FaFileExport />
+                Import From Excel
+              </button>
+            </div>
           </div>
-        </div>
-        <div className='flex flex-col justify-center w-full h-full grow'>
-          <PatientSearchBar filter={filter} setFilter={setFilter} />
           <EnhancedTable filter={filter} />
         </div>
       </main>
